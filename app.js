e55d5879e2b18e7b7f93bdb5a6f170c9cf0dc2c8
Milad <milad.rahimi1412@gmail.coml>
2022-06-17 00:03:46 +0430

    Deleted extra statements

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const arr3 = [2, 3, 4, 5, 6, 7];
 const arr5 = [1, 555];
 
 let container = [arr1, arr2, arr3, arr4, arr5, arr6];
-let sample = [];
-let counter = 0;
 
 for (let i = 0; i < container.length; i++) {
   for (let j = i + 1; j < container.length; j++) {
